e63aba35a3eecd78935b63ca08e9cc6ce98fe1ca
sunnytran <sunnydinhtran@gmail.com>
2019-10-20 22:38:13 -0500

    Added all text files

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,25 @@
 
+import java.io.File;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 	
-	public static void main(String args[]) {
+	public static void main(String args[]) throws Exception {
 		ParkingLot lot = new ParkingLot();
 		
-		for (int i = 0; i < 1000; i++) {
-			int carType = new Random().nextInt(3) + 1;
-			
-			if (carType == 1)
-				lot.park(new Sedan());
-			else if (carType == 2) 
-				lot.park(new SUV());
-			else
-				lot.park(new Truck());
-			
-			lot.tick();
+		Scanner scan = new Scanner(new File("parking-lot-info-5.txt"));
+		while (scan.hasNextLine()) {
+				String carType = scan.nextLine();
+				
+				if (carType.equals("Sedan"))
+					lot.park(new Sedan());
+				else if (carType.equals("SUV")) 
+					lot.park(new SUV());
+				else
+					lot.park(new Truck());
+				
+				lot.tick();
 		}
 		
 		System.out.println("Made $" + lot.getMoney());
